0d4cf5c3f03ca73b8b01965a401a8823f6517df0
Dainsleif <dainsleif@yeah.net>
2025-05-14 14:44:47 +0800

    Packaged dependencies

---
+++ src/main/java/top/offsetmonkey538/meshlib/mixin/ServerNetworkIoMixin.java
@@ -0,0 +1,22 @@
+package top.offsetmonkey538.meshlib.mixin;
+
+import io.netty.channel.Channel;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import top.offsetmonkey538.meshlib.impl.ProtocolHandler;
+
+import static top.offsetmonkey538.meshlib.impl.ProtocolHandler.MOD_ID;
+
+@Mixin(targets = "net/minecraft/server/ServerNetworkIo$1")
+public abstract class ServerNetworkIoMixin {
+
+    @Inject(
+            method = "initChannel",
+            at = @At("TAIL")
+    )
+    private void meshlib$addHttpHandler(Channel channel, CallbackInfo ci) {
+        channel.pipeline().addFirst(MOD_ID, new ProtocolHandler());
+    }
+}
--- src/main/java/top/syshub/loadResourcepackFromServer/LoadResourcepackFromServerPaper.java
+++ src/main/java/top/syshub/loadResourcepackFromServer/LoadResourcepackFromServerPaper.java
@@ -1,15 +1,18 @@
 package top.syshub.loadResourcepackFromServer;
 
+import io.papermc.paper.network.ChannelInitializeListenerHolder;
+import net.kyori.adventure.key.Key;
 import org.bukkit.plugin.java.JavaPlugin;
 import top.offsetmonkey538.meshlib.api.HttpHandlerRegistry;
+import top.offsetmonkey538.meshlib.impl.ProtocolHandler;
 
 public final class LoadResourcepackFromServerPaper extends JavaPlugin {
     @Override
     public void onEnable() {
+        ChannelInitializeListenerHolder.addListener(Key.key("meshlib", "meshlib"), (channel) -> channel.pipeline().addFirst("mesh-lib", new ProtocolHandler()));
         HttpHandlerRegistry.INSTANCE.register("resourcepack.zip", new ResourcePackHandler());
     }
     @Override
-    public void onDisable() {
-    }
+    public void onDisable() {}
 }
 
